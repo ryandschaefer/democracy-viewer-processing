62f9919d47c28b7fe8dfde403931533fa2a24cf8
Ryan Schaefer <rschaefer1@me.com>
2024-08-02 00:00:15 -0500

    Update email

--- util/email.py
+++ util/email.py
@@ -1,12 +1,12 @@
 from email.mime.text import MIMEText
-from smtplib import SMTP
+from smtplib import SMTP_SSL
 from os import environ
 from util.sql_connect import sql_connect
 from util.sql_queries import get_user
 
+engine, meta = sql_connect()
+
 def send_email(template: str, params: dict, subject: str, to: str):
-    engine, meta = sql_connect()
-    
     # Set name based on provided fields
     user = get_user(engine, meta, to)
     name = "{} {}".format(user["first_name"], user["last_name"])
@@ -16,14 +16,20 @@ def send_email(template: str, params: dict, subject: str, to: str):
         name = "{} {}".format(title, name)
     if suffix is not None:
         name = "{} {}".format(name, suffix)
+    params["name"] = name
     
     # Fill in remaining parameters in email template
     with open("util/emails/{}.txt".format(template), "r") as file:
-        text = file.read()
-        text = text.replace("[[name]]", name)
-        for key, val in params.items():
-            text = text.replace("[[{}]]".format(key), str(val))
-        message = MIMEText(text)
+        full_text = ""
+        for line in file.readlines():
+            if len(line) > 0:
+                text = "<p>{}</p>".format(line)
+                text = text.replace("[[name]]", name)
+                for key, val in params.items():
+                    text = text.replace("[[{}]]".format(key), str(val))
+                full_text += text    
+                
+        message = MIMEText(full_text, "html")
 
     # Set headers
     from_email = environ.get("FROM_EMAIL")
@@ -32,8 +38,8 @@ def send_email(template: str, params: dict, subject: str, to: str):
     message["To"] = to
     
     # Send email
-    server = SMTP(environ.get("EMAIL_SERVER"), int(environ.get("EMAIL_PORT")))
-    server.starttls()
+    server = SMTP_SSL(environ.get("EMAIL_SERVER"), int(environ.get("EMAIL_PORT")))
+    # server.set_debuglevel(1)
     server.login(from_email, environ.get("EMAIL_PASSWORD"))
     server.sendmail(from_email, to, message.as_string())
     server.quit()
